c7b1c07dab5fdac03d5f21cec8a5ae8adb81d2d1
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-22 17:20:36 +0200

    Emberek osztály - toString()
    Main
    - Emberek példányosítása és kiírása

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -5,8 +5,11 @@ public class Main {
         Ember e1 = new Ember("Gipsz Jakab", "1988-1-12", "Budapest");
         Ember e2 = new Ember("Teszt Elek", "2003-12-5", "Debrecen");
         Ember e3 = new Ember("Kovács István", "1973-9-22", "Pécs");
-        System.out.println(e1);
-        System.out.println(e2);
-        System.out.println(e3);
+        Ember e4 = new Ember("Lakatos József", "2005-7-15", "Veszprém");
+        Ember e5 = new Ember("Kovács Dóra", "1999-12-31", "Miskolc");
+        Ember[] emberTomb = new Ember[]{e1, e2, e3, e4, e5};
+        Emberek ek = new Emberek(emberTomb);
+        System.out.println(ek);
+
     }
 }
