2c30bd9ae440b271890f40bc394465ee5158f1c3
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-23 16:16:47 +0200

    Életkor meghatározása javítva

--- src/hu/petrik/emberekoop/Ember.java
+++ src/hu/petrik/emberekoop/Ember.java
@@ -1,6 +1,9 @@
 package hu.petrik.emberekoop;
 
+import javax.swing.text.DateFormatter;
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class Ember {
     private String nev;
@@ -13,27 +16,44 @@ public class Ember {
         this.szulHely = szulHely;
     }
 
-    public int getSzuletesiEv(){
-        return Integer.parseInt(this.szulDatum.substring(0,4));
+    public int getSzuletesiEv() {
+        return Integer.parseInt(this.szulDatum.substring(0, 4));
     }
 
-    public int getSzuletesiHonap(){
+    public int getSzuletesiHonap() {
         String[] szuletesiAdatok = this.szulDatum.split("-");
         return Integer.parseInt(szuletesiAdatok[1]);
     }
 
-    public int getSzuletesiNap(){
+    public int getSzuletesiNap() {
         String[] szuletesiAdatok = this.szulDatum.split("-");
         return Integer.parseInt(szuletesiAdatok[2]);
     }
 
     public int getEletkor() {
-        return LocalDate.now().getYear() - this.getSzuletesiEv();
+        LocalDate maiDatum = LocalDate.now();
+        boolean voltEIdenSzuletesnapja = maiDatum.getMonthValue() > getSzuletesiHonap()
+                || (maiDatum.getMonthValue() == getSzuletesiHonap() &&
+                maiDatum.getDayOfMonth() >= getSzuletesiNap());
+        int eletkor = LocalDate.now().getYear() - this.getSzuletesiEv();
+        if (!voltEIdenSzuletesnapja) {
+            eletkor--;
+        }
+        return eletkor;
+    }
+
+    public int getEletkor2() {
+        LocalDate maiDatum = LocalDate.now();
+        LocalDate szuletesiDatum = LocalDate.of(getSzuletesiEv(), getSzuletesiHonap(), getSzuletesiNap());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-M-d");
+        szuletesiDatum = LocalDate.parse(this.szulDatum, formatter);
+        Period eletkor = Period.between(szuletesiDatum, maiDatum);
+        return  eletkor.getYears();
     }
 
     @Override
     public String toString() {
         return String.format("%30s %10s (%3d év) %20s", this.nev, this.szulDatum,
-                this.getEletkor(), this.szulHely);
+                this.getEletkor2(), this.szulHely);
     }
 }
